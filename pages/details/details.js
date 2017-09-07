d1190fc83eee02159fb0f685b55605dcd28e340d
jiangniu <gdl_Applet@163.com>
2017-09-07 23:59:49 +0800

    09-07||23:59

--- pages/details/details.js
+++ pages/details/details.js
@@ -10,7 +10,7 @@ Page({
       'https://img.gegejia.com/activity/saleWindow/2e53490d15f87.jpg',
       'https://img.gegejia.com/activity/saleWindow/2e53490d15f87.jpg'
     ],
-    indicatorDots: false,
+    indicatorDots: true,
     autoplay: false,
     interval: 5000,
     duration: 1000,
@@ -27,10 +27,10 @@ Page({
       stock:"100个",
       weight:"900g",
       instruction:"补水就用水密码水力全开,唤醒”肤”君过七夕！水密码护肤品全场满199减100，肌不可失，为爱下单吧！【补水就用水密码】水力全开,唤醒”肤”君过七夕！水密码护肤品全场满199减100，肌不可失，为爱下单吧！"
-    }
-  },
-  onPullDownRefresh: function () {
-    wx.stopPullDownRefresh()
+    },
+    //
+    num: 1, // input默认是1   
+    minusStatus: 'disabled' // 使用data数据对象设置样式名
   },
   onShareAppMessage :function(res){
     if (res.from === 'button') {
@@ -42,7 +42,7 @@ Page({
       path: 'pages/details/details',
       imageUrl:'http://overwatch.nos.netease.com/1/assets/images/hero/doomfist/icon-portrait.png',
       success: function (res) {
-        // 转发成功
+        //s 转发成功
       },
       fail: function (res) {
         // 转发失败
@@ -54,6 +54,34 @@ Page({
       // url:"../loginRegister/login/login"
       url:"/pages/order/order"
     })
+  },
+  /* 点击减号 */
+  bindMinus: function () {
+    var num = this.data.num;
+    // 如果大于1时，才可以减  
+    if (num > 1) {
+      num--;
+    }
+    // 只有大于一件的时候，才能normal状态，否则disable状态  
+    var minusStatus = num <= 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回  
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
+  },
+  /* 点击加号 */
+  bindPlus: function () {
+    var num = this.data.num;
+    // 不作过多考虑自增1  
+    num++;
+    // 只有大于一件的时候，才能normal状态，否则disable状态  
+    var minusStatus = num < 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回  
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
   },
   /**
    * 生命周期函数--监听页面加载
